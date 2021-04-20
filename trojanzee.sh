d93e4194535981e712c0c12785cb2cf2b14f2e94
QueenProJect <78314626+BiiDev@users.noreply.github.com>
2021-04-20 20:42:51 +0700

    Add files via upload

---
+++ trojanzee.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#Apa mau reedit?izin dlu goblok awas kau kalo asal
+#reedit gua sumpahin lumpuh kakilu selamanya!!!
+
+clear
+echo -e "\e[91m"
+xdg-open https://youtube.com/channel/UC2Fhe50mzSFpJTWRD44O5rw
+figlet "Z-TX TOOLS"
+echo -e "\e[33m===================="
+echo -e "\e[95mAUTHOR = Zee TeExx"
+echo -e "\e[95mYOUTUBE = Z-TX "
+echo -e "\e[33m===================="
+
+
+sleep 2
+echo -e "\e[91m"
+read -p 'NOMOR HP TARGET = ' nomor
+while [[ true ]]
+do
+echo -e "\e[38;5;82mVirus Telah di Kirim By Zee TeExx"
+done
